c203188aeb0e5e5e16ed7efe68a90c60cf09db57
John Comeau <jc@unternet.net>
2022-04-06 05:51:17 -0600

    starting to take shape

--- ps3d.py
+++ ps3d.py
@@ -4,7 +4,10 @@ Prototype of a 3D extension of postscript
 
 Produces .obj files for 3d printing
 '''
-import sys
+import sys, logging  # pylint: disable=multiple-imports
+from ast import literal_eval
+
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 STACK = []
 
@@ -16,5 +19,33 @@ def convert(infile=sys.stdin, outfile=sys.stdout):
         infile = open(infile)
     if outfile != sys.stdout:
         outfile = open(outfile, 'w')
+    words = ps3d()
+    shebang = next(infile)
+    if not shebang.startswith('%!ps3d'):
+        raise ValueError('valid input should start with "%!ps3d"')
+    for line in infile:
+        tokens = line.split()
+        for token in tokens:
+            line = line.lstrip()[len(token):]
+            if token.startswith('%'):
+                outfile.write('# ' + token[1:] + line)
+                continue
+            if token in words:
+                words[token]()
+            else:
+                STACK.append(literal_eval(token))
+            logging.debug('STACK: %s', STACK)
+    infile.close()
+    outfile.close()
+
+def ps3d():
+    '''
+    words which define the ps3d language
+    '''
+    # pylint: disable=possibly-unused-variable
+    def add():
+        STACK.append(STACK.pop() + STACK.pop())
+    return locals()
+
 if __name__ == '__main__':
     convert(*sys.argv[1:])
