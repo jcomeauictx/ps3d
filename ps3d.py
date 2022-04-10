c7c479e307776ae9458cd61eecc0e06b9f54f4ad
John Comeau <jc@unternet.net>
2022-04-09 21:34:13 -0600

    that fixed bottom of quadrant0 segment, quadrant3 still borked

--- ps3d.py
+++ ps3d.py
@@ -309,10 +309,10 @@ def ps3d():
             faces = {
                 'top': (vertices[i - 1] + 1 for i in [1, 2, 3, 4]),
                 'bottom': (vertices[i - 1] + 1 for i in [8, 7, 6, 5]),
-                'left': (vertices[i - 1] + 1 for i in [4, 8, 5, 1]),
-                'right': (vertices[i - 1] + 1 for i in [2, 6, 7, 3]),
-                'start': (vertices[i - 1] + 1 for i in [1, 5, 6, 2]),
-                'end': (vertices[i - 1] + 1 for i in [3, 7, 8, 4]),
+                'left': (vertices[i - 1] + 1 for i in [5, 6, 2, 1]),
+                'right': (vertices[i - 1] + 1 for i in [4, 3, 7, 8]),
+                'start': (vertices[i - 1] + 1 for i in [2, 6, 7, 3]),
+                'end': (vertices[i - 1] + 1 for i in [5, 1, 4, 8]),
             }
             return faces
 
